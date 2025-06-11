840703e6b21c774b3038418511605cdfafbe7820
Alfred <alfred-brannarer@hotmail.com>
2025-06-11 14:42:59 +0200

    refactor: move Hero completely into Hero.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,10 @@
-import { Hero } from '@/components/sections';
+import { Hero, Skills } from '@/components/sections';
 
 export default function Home() {
   return (
     <main>
-      <Hero
-        title="Hello,"
-        description={
-          <>
-            My name is{' '}
-            <span className="text-amber-500 font-semibold">
-              Alfred
-            </span>
-            , a{' '}
-            <span className="text-amber-500 font-semibold">
-              Software Development
-            </span>{' '}
-            student at Lernia specializing in{' '}
-            <span className="text-amber-500 font-semibold">
-              Java
-            </span>{' '}
-            and{' '}
-            <span className="text-amber-500 font-semibold">
-              JavaScript
-            </span>
-          </>
-        }
-        content="I'm passionate about building a strong foundation in both frontend and backend technologies to create effective and user-friendly applications. I'm eager to continue expanding my expertise."
-        imageUrl="/images/portrait.webp"
-        imageAlt="Portrait of Alfred Brännare"
-      />
+      <Hero />
+      <Skills />
     </main>
   );
 }
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,51 +1,56 @@
 import type { FC } from 'react';
 import Image from 'next/image';
 import { Separator } from '@/components/ui/separator';
-import { type HeroCardProps } from '@/types';
 
-const Hero: FC<HeroCardProps> = ({
-  title,
-  description,
-  content,
-  imageUrl,
-  imageAlt,
-}) => {
+const Hero: FC = () => {
   return (
     <section className="flex justify-center">
       <div>
         <div className="flex flex-col text-center items-center lg:flex-row lg:text-start">
           <div className="flex-1 flex flex-col justify-center space-y-4 p-4">
             <h1 className="text-5xl font-bold text-amber-500">
-              {title}
+              Hello,
             </h1>
             <p className="text-2xl font-semibold">
-              {description}
+              My name is{' '}
+              <span className="text-amber-500 font-semibold">
+                Alfred
+              </span>
+              , a{' '}
+              <span className="text-amber-500 font-semibold">
+                Software Development
+              </span>{' '}
+              student at Lernia specializing in{' '}
+              <span className="text-amber-500 font-semibold">
+                Java
+              </span>{' '}
+              and{' '}
+              <span className="text-amber-500 font-semibold">
+                JavaScript
+              </span>
             </p>
             <div className="space-y-2">
-              {typeof content === 'string' ? (
-                content
-                  .split('. ')
-                  .map(
-                    (sentence: string, index: number) => (
-                      <p key={index}>{sentence.trim()}.</p>
-                    )
-                  )
-              ) : (
-                <div>{content}</div>
-              )}
+              <p>
+                I&apos;m passionate about building a strong
+                foundation in both frontend and backend
+                technologies to create effective and
+                user-friendly applications.
+              </p>
+              <p>
+                I&apos;m eager to continue expanding my
+                expertise.
+              </p>
             </div>
           </div>
 
-          {imageUrl && (
-            <Image
-              src={imageUrl}
-              width={450}
-              height={450}
-              alt={imageAlt ?? 'Hero image'}
-              priority
-              className="rounded-lg object-cover"
-            />
-          )}
+          <Image
+            src="/images/portrait.webp"
+            width={450}
+            height={450}
+            alt="Portrait of Alfred Brännare"
+            priority
+            className="rounded-lg object-cover"
+          />
         </div>
         <Separator className="bg-stone-200 h-1" />
       </div>
