f3bd5418d01dfff2278b2492190fa780777516d1
Alfred <alfred-brannarer@hotmail.com>
2025-06-14 12:20:49 +0200

    feat: finalize timeline of projects

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -25,17 +25,22 @@ const Projects = () => {
         className="w-full"
       >
         <CarouselContent>
-          {sortedProjects.map((project) => (
+          {sortedProjects.map((project, index) => (
             <CarouselItem
               className="basis-full sm:basis-1/2 lg:basis-1/3"
               key={project.id}
             >
               <div className="flex flex-col justify-center items-center text-center">
                 <span>{project.date}</span>
-                <span
-                  className="bg-amber-400 w-5 h-5 rounded-full block my-6"
-                  aria-hidden="true"
-                />
+                <div className="relative flex items-center justify-center w-full">
+                  {index < sortedProjects.length - 1 && (
+                    <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 w-[calc(100%+1.5rem)] h-0.5 bg-amber-400 z-0" />
+                  )}
+                  <span
+                    className="bg-amber-400 w-5 h-5 rounded-full block my-6"
+                    aria-hidden="true"
+                  />
+                </div>
                 <span className="mb-2 items-center text-xs md:text-xl">
                   {project.title}
                 </span>
