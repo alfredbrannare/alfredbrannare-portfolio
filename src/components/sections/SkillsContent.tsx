1463b7a56632a0f36265a595723d7bc4437da3ca
Alfred Brännare <alfred-brannare@hotmail.com>
2025-10-06 13:45:35 +0200

    style: remove x padding from Skills section

--- src/components/sections/SkillsContent.tsx
+++ src/components/sections/SkillsContent.tsx
@@ -25,7 +25,7 @@ export const SkillsContent = ({
 
   return (
     <section
-      className="flex flex-col justify-center items-center mt-6 px-4"
+      className="flex flex-col justify-center items-center mt-6"
       id="techstack"
     >
       <h1 className="text-5xl font-bold text-amber-500 mb-6 text-shadow-[1px_1px_0_rgba(0,0,0,0.7)]">
