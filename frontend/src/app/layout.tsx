70a0b4150e6a3ad0caa9700e9fa5d43a618ad1ce
Alfred Brännare <alfred-brannare@hotmail.com>
2026-06-05 10:09:19 +0200

    feat(frontend): add boilerplate footer without content

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -42,6 +43,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Header />
         <TooltipProvider>{children}</TooltipProvider>
+        <Footer />
       </body>
     </html>
   );
---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-zinc-950">
+      <p></p>
+    </footer>
+  );
+}
